2cfac10f0ce928a58d349ce8278b52fde182b0d8
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-05-09 19:01:04 -0300

    test: Testar o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+result = (
+    "O produto Relâmpago Marquinhos "
+    "fabricado em 25/04/2023 "
+    "por Copa Pistão com validade "
+    "até 25/04/2033 "
+    "precisa ser armazenado LOCAL SECO E AREJADO."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Relâmpago Marquinhos",
+        nome_da_empresa="Copa Pistão",
+        data_de_fabricacao="25/04/2023",
+        data_de_validade="25/04/2033",
+        numero_de_serie="RM95",
+        instrucoes_de_armazenamento="LOCAL SECO E AREJADO",
+    )
+    assert repr(product) == result
