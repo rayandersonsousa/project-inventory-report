4821b77e3cc6426efd9001eff2786d8ab32b1e90
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-05-09 18:26:48 -0300

    feat: Gere os relatórios através de um arquivo JSON

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,12 @@
+from .importer import Importer
+import json
+
+
+class JsonImporter(Importer):
+    @staticmethod
+    def import_data(path):
+        if path.endswith(".json"):
+            with open(path) as file:
+                return json.load(file)
+
+        raise ValueError("Error!!")
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,12 +1,13 @@
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.json_importer import JsonImporter
 
 
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        file_data = CsvImporter.import_data(path)
+        file_data = Inventory.file_type(path)
 
         if type == "simples":
             return SimpleReport.generate(file_data)
@@ -14,3 +15,10 @@ class Inventory:
             return CompleteReport.generate(file_data)
 
         raise ValueError("Error!!!")
+
+    @staticmethod
+    def file_type(path):
+        if path.endswith(".csv"):
+            return CsvImporter.import_data(path)
+        if path.endswith(".json"):
+            return JsonImporter.import_data(path)
