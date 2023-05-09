47f5f8dae622ebb088a9c1894b924f7d2d1e8781
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-05-09 16:37:37 -0300

    feat: Gerar a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        part_report = ""
+
+        inventory = Counter(
+            product["nome_da_empresa"]
+            for product in products
+        )
+
+        for enterprise, quantity in inventory.items():
+            part_report += f"- {enterprise}: {quantity}\n"
+
+        return (
+            f"{SimpleReport.generate(products)}\n"
+            "Produtos estocados por empresa:\n"
+            f"{part_report}"
+        )
